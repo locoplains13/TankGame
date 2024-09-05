25f757b1cb7c0539fa08ed8efd0beea9b07a7d9a
Angel Roberto Andrade <alroc1234@hotmail.com>
2024-09-05 17:13:40 -0600

    player moves with wasd and changes direction depending on where it moves

--- Player.gd
+++ Player.gd
@@ -1,11 +1,29 @@
-extends Node3D
+extends CharacterBody3D
 
+@export var speed = 10
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var targetVelocity = Vector3.ZERO
 
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("right"):
+		direction.z -= 1
+		print("moving right")
+	if Input.is_action_pressed("left"):
+		direction.z += 1
+	if Input.is_action_pressed("down"):
+		direction.x += 1
+	if Input.is_action_pressed("up"):
+		direction.x -= 1
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		
+		$Pivot.basis = Basis.looking_at(direction)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	targetVelocity.x = direction.x * speed
+	targetVelocity.z = direction.z * speed
+	
+	velocity = targetVelocity
+	move_and_slide()
