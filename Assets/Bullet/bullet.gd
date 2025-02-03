68f4e2cd7a3d2918117420edeaf24d88f62afd48
Angel Roberto Andrade <alroc1234@hotmail.com>
2025-02-02 22:56:59 -0800

    added trail to bullets

--- Assets/Bullet/bullet.gd
+++ Assets/Bullet/bullet.gd
@@ -1,9 +1,10 @@
-extends Node3D
+extends CharacterBody3D
 
-const SPEED = 20.0
+const SPEED = 30
 
 @onready var mesh = $MeshInstance3D
 @onready var ray = $RayCast3D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,7 +12,9 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position += transform.basis * Vector3(0, 0, -SPEED) * delta
+func _physics_process(delta: float) -> void:
+	position += global_basis * Vector3.FORWARD * SPEED * delta
 	if ray.is_colliding():
+		var position_normal = ray.get_collision_normal()
+		velocity = velocity.bounce(position_normal)
 		queue_free()
